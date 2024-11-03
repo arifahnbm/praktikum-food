1f4973ad3831143bb34982ac0e4835faf44299d0
arifahnbm <nura60583@gmail.com>
2024-11-03 22:07:41 +0700

    fifth

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark osahan-nav">
     <div class="container">
-       <a class="navbar-brand" href="index.html"><img alt="logo" src="img/favicon.png"></a>
+       <a class="navbar-brand" href="index.html"><img alt="logo" src="{{ asset('frontend/img/favicon.png') }}"></a>
        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
        <span class="navbar-toggler-icon"></span>
        </button>
@@ -37,7 +37,7 @@
              </li>
              <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <img alt="Generic placeholder image" src="img/user/4.png" class="nav-osahan-pic rounded-pill"> My Account
+                <img alt="Generic placeholder image" src="{{ asset('frontend/img/user/4.png') }}" class="nav-osahan-pic rounded-pill"> My Account
                 </a>
                 <div class="dropdown-menu dropdown-menu-right shadow-sm border-0">
                    <a class="dropdown-item" href="orders.html#orders"><i class="icofont-food-cart"></i> Orders</a>
