9b03a5d33961ec4fbccc44d4201218af85cad387
arifahnbm <nura60583@gmail.com>
2024-11-09 22:07:10 +0700

    eightth

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -4,8 +4,21 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Category;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class CategoryController extends Controller
 {
-    //
+    public function AllCategory() {
+        $category = Category::latest()->get();
+        return view('admin.backend.category.all_category', compact('category'));
+    }
+    //End method
+
+    public function AddCategory() {
+        return view('admin.backend.category.add_category');
+    }
+    //End method
 }
