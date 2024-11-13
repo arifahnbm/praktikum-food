b94b680547d1fd0042b399c75cf5fcb16f4c868d
arifahnbm <nura60583@gmail.com>
2024-11-13 08:11:06 +0700

    ninth

--- resources/views/admin/backend/category/edit_category.blade.php
+++ resources/views/admin/backend/category/edit_category.blade.php
@@ -9,12 +9,12 @@
         <div class="row">
             <div class="col-12">
                 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                    <h4 class="mb-sm-0 font-size-18">Add Category</h4>
+                    <h4 class="mb-sm-0 font-size-18">Edit Category</h4>
 
                     <div class="page-title-right">
                         <ol class="breadcrumb m-0">
                             <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboard</a></li>
-                            <li class="breadcrumb-item active">Add Category</li>
+                            <li class="breadcrumb-item active">Edit Category</li>
                         </ol>
                     </div>
 
@@ -28,15 +28,17 @@
                 <div class="card">
                 <div class="card-body p-4">
 
-                    <form id="myForm" action="{{ route('category.store') }}" method="post" enctype="multipart/form-data">
+                    <form id="myForm" action="{{ route('category.update') }}" method="post" enctype="multipart/form-data">
                         @csrf
+
+                        <input type="hidden" name="id" value="{{ $category->id }}">
 
                     <div class="row">
                         <div class="col-lg-12">
                             <div>
                                 <div class="form-group mb-3">
                                     <label for="example-text-input" class="form-label">Category Name</label>
-                                    <input class="form-control" name="category_name" type="text" id="example-text-input">
+                                    <input class="form-control" name="category_name" value="{{ $category->category_name }}" type="text" id="example-text-input">
                                 </div>
                                 
                             </div>
@@ -49,7 +51,7 @@
                                     <input class="form-control" name="image" type="file" id="image">
                                 </div>
                                 <div class="mb-3">
-                                    <img id="showImage" src="{{ url('upload/no_image.jpg') }}" alt="" class="rounded-circle p-1 bg-primary" width="110">
+                                    <img id="showImage" src="{{ asset($category->image) }}" alt="" class="rounded-circle p-1 bg-primary" width="110">
                                 </div>
                                 <div class="mt-4">
                                     <button type="submit" class="btn btn-primary waves-effect waves-light">Save Changes</button>
@@ -96,19 +98,13 @@
                 category_name: {
                     required : true,
                 }, 
-                image: {
-                    required : true,
-                }, 
                 
             },
             messages :{
                 category_name: {
                     required : 'Please Enter Category Name',
                 }, 
-                image: {
-                    required : 'Please Select Image',
-                }, 
-                 
+                
 
             },
             errorElement : 'span', 
--- routes/web.php
+++ routes/web.php
@@ -67,6 +67,19 @@ Route::middleware('admin')->group(function () {
     Route::controller(CategoryController::class)->group(function(){
         Route::get('/all/category', 'AllCategory')->name('all.category');
         Route::get('/add/category', 'AddCategory')->name('add.category');
+        Route::post('/store/category', 'StoreCategory')->name('category.store');
+        Route::get('/edit/category/{id}', 'EditCategory')->name('edit.category');
+        Route::post('/update/category', 'UpdateCategory')->name('category.update');
+        Route::get('/delete/category/{id}', 'DeleteCategory')->name('delete.category');
+    });
+
+    Route::controller(CategoryController::class)->group(function(){
+        Route::get('/all/city', 'AllCity')->name('all.city');
+        Route::get('/add/category', 'AddCategory')->name('add.category');
+        Route::post('/store/city', 'StoreCity')->name('city.store');
+        Route::get('/edit/city/{id}', 'EditCity');
+        Route::post('/update/city', 'UpdateCity')->name('city.update');
+        Route::get('/delete/city/{id}', 'DeleteCity')->name('delete.city');
     });
     
     
